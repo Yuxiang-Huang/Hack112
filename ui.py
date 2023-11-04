a5f6ec1a699cb15eee28e306cfa808fa3a7e4ff2
qianxuege <qianxuege15@gmail.com>
2023-11-04 15:43:48 -0400

    added camera boxes, the field, and the flags as rectangle

---
+++ ui.py
@@ -0,0 +1,45 @@
+from cmu_graphics import *
+
+def onAppStart(app):
+    app.width = 1200
+    app.height = 700
+    app.cameraBoxWidth=400
+    app.cameraBoxHeight=200
+    app.fieldDimensions = [0, app.cameraBoxHeight, app.width, app.height-app.cameraBoxHeight] # topLeftX, topLeftY, width, height
+    
+    app.flagHeight = 200
+    app.flagWidth = 100
+
+    app.url = 'https://academy.cs.cmu.edu/static/media/project_10.472f439f.jpg'
+
+def redrawAll(app):
+    #camera boxes
+    drawRect(0, 0, app.cameraBoxWidth, app.cameraBoxHeight, border="black", fill=None)
+    drawRect(app.width-app.cameraBoxWidth, 0, app.cameraBoxWidth, app.cameraBoxHeight, border="black", fill=None)
+    
+    #field
+    drawRect(app.fieldDimensions[0], app.fieldDimensions[1], app.fieldDimensions[2], app.fieldDimensions[3], fill="lightGreen")
+    
+    #flags
+    drawFlags(app)
+    
+    #player
+    
+    
+    
+    # imageWidth, imageHeight = getImageSize(app.url)
+
+    # drawLabel(f'Original ({imageWidth}x{imageHeight})', 125, 75, size=16)
+    # drawImage(app.url, 125, 200, align='center')
+def drawFlags(app):
+    topFlagY = app.cameraBoxHeight + ((app.fieldDimensions[3] - 200)//2)
+    rightFlagX = app.width - app.flagWidth
+
+    drawRect(0, topFlagY, app.flagWidth, app.flagHeight, fill="red")
+    drawRect(rightFlagX, topFlagY, app.flagWidth, app.flagHeight, fill="red")
+
+
+def main():
+  runApp()
+
+main()
