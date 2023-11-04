49a189f53f6f520c01187a3129701af091c3137b
qianxuege <qianxuege15@gmail.com>
2023-11-04 19:56:53 -0400

    added images of obstacles and wrote the drawImage function

--- ui.py
+++ ui.py
@@ -36,3 +36,7 @@ def drawScreen(app):
 
     # vertical center line
     drawLine(app.width / 2, 0, app.width / 2, app.height, fill="black")
+    
+def oceanTheme():
+    colorPalette = {"oceanBlue": rgb(95, 123, 165), "rock": rgb(73, 70, 67)}
+    procreateBrush = {"rock": "Syrup"}
